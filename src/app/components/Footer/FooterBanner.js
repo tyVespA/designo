521da89632e56b35ad0e4ad210a9abecb0f14829
tyVespA <migliavacca.marco08@gmail.com>
2024-12-18 15:45:33 +0100

    Add bg, fix layouts

--- src/app/components/Footer/FooterBanner.js
+++ src/app/components/Footer/FooterBanner.js
@@ -1,19 +1,26 @@
+import Image from "next/image";
 import styles from "./FooterBanner.module.css";
 import Button from "../Button";
+import bg_pattern from "../../../../public/shared/desktop/bg-pattern-call-to-action.svg";
 
 export default function FooterBanner() {
   return (
     <div className={styles.footerBannerContainer}>
-      <div className={styles.text}>
-        <h2>Parliamo del tuo progetto</h2>
-        <p>
-          Pronto a portare il tuo progetto al livello successivo? Contattaci
-          oggi stesso e scopri come la nostra esperienza può aiutare la tua
-          attività a crescere.
-        </p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <Button theme="light">Contattaci</Button>
+      <div className={styles.footerBanner}>
+        <div className={styles.bgContainer}>
+          <Image src={bg_pattern} alt="" />
+        </div>
+        <div className={styles.text}>
+          <h2>Parliamo del tuo progetto</h2>
+          <p>
+            Pronto a portare il tuo progetto al livello successivo? Contattaci
+            oggi stesso e scopri come la nostra esperienza può aiutare la tua
+            attività a crescere.
+          </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button theme="light">Contattaci</Button>
+        </div>
       </div>
     </div>
   );
