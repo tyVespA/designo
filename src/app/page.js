dbc5f89716cf5117f28f1295f393bf1e81a63220
tyVespA <migliavacca.marco08@gmail.com>
2024-12-18 02:09:17 +0100

    Add section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 import Button from "./components/Button";
+import LinkCard from "./components/LinkCard";
 
 import bg_pattern_hero_desktop from "../../public/home/desktop/bg-pattern-hero-home.svg";
 import image_hero_phone_desktop from "../../public/home/desktop/image-hero-phone.png";
+import image_web_design_desktop from "../../public/home/desktop/image-web-design-large.jpg";
 
 export default function Home() {
   return (
@@ -33,6 +36,29 @@ export default function Home() {
             className={styles.bgPattern}
           />
         </div>
+      </section>
+
+      <section className={styles.linksCardsSection}>
+        <div className={styles.item1}>
+          <LinkCard
+            href="/"
+            title="Web design"
+            imageSrc={image_web_design_desktop}
+            alt="alt"
+          />
+        </div>
+        <LinkCard
+          href="/"
+          title="App design"
+          imageSrc={image_web_design_desktop}
+          alt="alt"
+        />
+        <LinkCard
+          href="/"
+          title="Graphic design"
+          imageSrc={image_web_design_desktop}
+          alt="alt"
+        />
       </section>
     </>
   );
