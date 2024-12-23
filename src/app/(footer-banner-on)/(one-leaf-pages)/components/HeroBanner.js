66f998e1aecce61f770d3fbd824b465c22e4d6eb
tyVespA <migliavacca.marco08@gmail.com>
2024-12-23 11:28:49 +0100

    Add variable background image, add media queries

--- src/app/(footer-banner-on)/(one-leaf-pages)/components/HeroBanner.js
+++ src/app/(footer-banner-on)/(one-leaf-pages)/components/HeroBanner.js
@@ -1,8 +1,27 @@
+import Image from "next/image";
 import styles from "./HeroBanner.module.css";
 
-export default function HeroBanner({ title, children, backgroundImgPosition }) {
+import bg_web from "../../../../../public/web-design/desktop/bg-pattern-intro-web.svg";
+import bg_app from "../../../../../public/app-design/desktop/bg-pattern-intro-app.svg";
+import bg_graphic from "../../../../../public/graphic-design/desktop/bg-pattern-intro-graphic.svg";
+
+export default function HeroBanner({ title, children }) {
+  let titleCheck = title.toLowerCase();
+  let bgImage;
+  switch (titleCheck) {
+    case "web design":
+      bgImage = bg_web;
+      break;
+    case "app design":
+      bgImage = bg_app;
+      break;
+    case "graphic design":
+      bgImage = bg_graphic;
+      break;
+  }
   return (
     <section className={styles.heroBannerContainer}>
+      <Image src={bgImage} alt="" className={styles.bgImage} />
       <h1>{title}</h1>
       <p>{children}</p>
     </section>
