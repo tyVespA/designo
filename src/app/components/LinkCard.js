9936ede59a03d2ce0b28cc7346b3fcb7f135fcbf
tyVespA <migliavacca.marco08@gmail.com>
2024-12-23 09:28:11 +0100

    Add function to simplify component usage

--- src/app/components/LinkCard.js
+++ src/app/components/LinkCard.js
@@ -4,7 +4,42 @@ import Image from "next/image";
 
 import arrow from "../../../public/arrow.svg";
 
-export default function LinkCard({ href, title, imageSrc, alt }) {
+import web_design from "../../../public/home/desktop/image-web-design-large.jpg";
+import app_design from "../../../public/home/desktop/image-app-design.jpg";
+import graphic_design from "../../../public/home/desktop/image-graphic-design.jpg";
+
+export default function LinkCard({ title }) {
+  function getVariables(title) {
+    title = title.toLowerCase();
+    let href, imageSrc, alt;
+
+    switch (title) {
+      case "app design":
+        href = "/app-design";
+        imageSrc = app_design;
+        alt = "App Design";
+        break;
+      case "graphic design":
+        href = "/graphic-design";
+        imageSrc = graphic_design;
+        alt = "Graphic Design";
+        break;
+      case "web design":
+        href = "/web-design";
+        imageSrc = web_design;
+        alt = "Web Design on a Laptop";
+        break;
+      default:
+        href = "/";
+        imageSrc = null;
+        alt = "Default description";
+    }
+
+    return { href, imageSrc, alt };
+  }
+
+  const { href, imageSrc, alt } = getVariables(title);
+
   return (
     <Link href={href} className={styles.linkCard}>
       <h2>{title}</h2>
