7b0243601f0cb8990752fb1ed65b5bd436606b09
tyVespA <migliavacca.marco08@gmail.com>
2024-12-24 06:18:24 +0100

    Create and style

---
+++ src/app/components/Locations/LocationsSection.js
@@ -0,0 +1,31 @@
+import styles from "./LocationsSection.module.css";
+import Location from "./Location";
+
+import illustration_canada from "../../../../public/shared/desktop/illustration-canada.svg";
+import illustration_australia from "../../../../public/shared/desktop/illustration-australia.svg";
+import illustration_united_kingdom from "../../../../public/shared/desktop/illustration-united-kingdom.svg";
+
+export default function LocationsSection() {
+  return (
+    <section className={styles.locationsSection}>
+      <Location
+        imgName={illustration_canada}
+        title="Canada"
+        alt="illustrazione canada"
+        bgRotation={90}
+      />
+      <Location
+        imgName={illustration_australia}
+        title="Australia"
+        alt="illustrazione Australia"
+        bgRotation={0}
+      />
+      <Location
+        imgName={illustration_united_kingdom}
+        title="Regno Unito"
+        alt="illustrazione Regno Unito"
+        bgRotation={270}
+      />
+    </section>
+  );
+}
