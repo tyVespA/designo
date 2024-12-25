ebd165eb7e51d16d287c0f9219df37060872cc4b
tyVespA <migliavacca.marco08@gmail.com>
2024-12-25 09:44:48 +0100

    Add bgs, add media queries

--- src/app/(footer-banner-off)/contatti/ContattaciBanner.js
+++ src/app/(footer-banner-off)/contatti/ContattaciBanner.js
@@ -1,15 +1,23 @@
 import styles from "./ContattaciBanner.module.css";
+import ContattaciForm from "./ContattaciForm";
 import Image from "next/image";
 
-import bg_pattern_hero_deskop from "../../../../public/contact/desktop/bg-pattern-hero-desktop.svg";
+import bg_pattern_hero_desktop from "../../../../public/contact/desktop/bg-pattern-hero-desktop.svg";
+import bg_pattern_hero_mobile from "../../../../public/contact/mobile/bg-pattern-hero-contact-mobile.svg";
 
 export default function ContattaciBanner() {
   return (
     <section className={styles.contattaciContainer}>
       <Image
-        src={bg_pattern_hero_deskop}
+        src={bg_pattern_hero_desktop}
         alt=""
-        className={styles.bg_pattern_hero_deskop}
+        className={styles.bg_pattern_hero_desktop}
+        priority
+      />
+      <Image
+        src={bg_pattern_hero_mobile}
+        alt=""
+        className={styles.bg_pattern_hero_mobile}
         priority
       />
       <div className={styles.textSection}>
@@ -21,7 +29,9 @@ export default function ContattaciBanner() {
           utenti, contattaci!
         </p>
       </div>
-      <div className={styles.formSection}>form</div>
+      <div className={styles.formSection}>
+        <ContattaciForm />
+      </div>
     </section>
   );
 }
